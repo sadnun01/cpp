0342bb9d9020d1fe61e0e0283e35e25feeb9305d
NABIAL NAHIN SADNUN <nabialnahinsadnun01@gmail.com>
2024-12-18 18:58:52 +0600

    Add files via upload

---
+++ nastedLoops.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 10;
+    for (int i = 1; i <= n; i++) { // Fixed the loop syntax
+        int m = 10;
+        for (int j = 1; j <= m; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
